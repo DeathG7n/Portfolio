a5e994a62f5b6e1ea8c4ef282a3dfc2ac959a549
DeathG7n <christariccykid55@gmail.com>
2022-06-13 23:26:59 +0100

    Portfolio Website

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portfolio
+
+This is a basic portfolio website made to show my authenticity.
+
+This website was made with HTML, CSS and Javascript. 
+
+It was made responsive on all the popular devices.
---
+++ src/app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let x = document.getElementById("nav-bar__links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block"
+    }
+  }
